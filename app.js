c9a434a4386d414cd167e206a5356fa2de07ce39
cdobru <cristian.dobru@snyk.io>
2024-06-19 12:14:17 +0300

    asda

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var dust = require('dustjs-linkedin');
 var dustHelpers = require('dustjs-helpers');
 var cons = require('consolidate');
 const hbs = require('hbs')
-var app = express();
+// var app = express();
 app.use(helmet());
 var routes = require('./routes');
 var routesUsers = require('./routes/users.js')
